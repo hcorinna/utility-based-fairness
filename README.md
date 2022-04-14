fb4209d99f4a75092eebf6291d1700d85a4a23b8
hcorinna <12029382+hcorinna@users.noreply.github.com>
2022-04-14 03:43:01 +0200

    Clean up, started implementing plots

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Utility-based fairness definitions
+
 :wrench: Private implementation of the extension of group
+
+## Requirements
+
+To install requirements, you should have Anaconda installed. Once installed, run:
+
+```setup
+conda env create -f environment.yml
+conda activate utility-based-fairness
+```
--- evaluate_thresholds.py
+++ evaluate_thresholds.py
@@ -3,18 +3,14 @@ import config_data
 import train
 import data
 import calculate_utilities
-import calculate_fairnessscore
+import pattern_functions
+import oapackage
 
 def to_threshold(r, number_of_thresholds):
     threshold = (1/(number_of_thresholds-1)) * r
     return round(threshold, 2)
 
-def evaluate_thresholds():
-    dataset_info = config_data.chosen_dataset
-    X_train, X_test, y_train, y_test = data.get_train_test_data(dataset_info)
-    clf = train.train_lr(dataset_info)
-    all_data = train.get_predictions(clf, X_train, y_train)
-
+def evaluate_model(all_data):
     U_DM = []
     U_DS_A0 = []
     U_DS_A1 = []
@@ -27,7 +23,37 @@ def evaluate_thresholds():
         U_DS_A0.append(U_DS_r_A0)
         U_DS_r_A1 = calculate_utilities.U_DM(all_data, 1, r)
         U_DS_A1.append(U_DS_r_A1)
-        compare_utilities_function = calculate_fairnessscore.get_pattern_function()
+        compare_utilities_function = pattern_functions.get_pattern_function()
         FS_r = compare_utilities_function(U_DS_r_A0, U_DS_r_A1)
         FS.append(FS_r)
     return U_DM, U_DS_A0, U_DS_A1, FS
+
+def calculate_pareto_front(FS, U_DM):
+    pareto=oapackage.ParetoDoubleLong()
+
+    for i in range(len(FS)):
+        w = oapackage.doubleVector((FS[i], U_DM[i]))
+        pareto.addvalue(w, i)
+
+    pareto.show(verbose=1)
+    
+    pareto_list = pareto.allindices()
+    return pareto_list
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+dataset_info = config_data.chosen_dataset
+X_train, X_test, y_train, y_test = data.get_train_test_data(dataset_info)
+clf = train.train_lr(dataset_info)
+all_data = train.get_predictions(clf, X_train, y_train)
+
+U_DM, U_DS_A0, U_DS_A1, FS = evaluate_model(all_data)
+pareto_front_indices = calculate_pareto_front(FS, U_DM)
+pareto_list = list(pareto_front_indices)
+plt.plot(np.array(FS)[pareto_list].tolist(), np.array(U_DM)[pareto_list].tolist())
+# TODO instead of red, iterate over color map and use those colors for bar chart
+color_values = ['red' if p in pareto_front_indices else 'black' for p in range(len(FS))]
+plt.scatter(FS, U_DM, c=color_values)
+
+# TODO bar charts in different colors for DS utilities of pareto front
